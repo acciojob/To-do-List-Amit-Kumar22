e1d0d4a01c193002be08b93d63bd4586159bdc43
Amit-Kumar22 <135856138+Amit-Kumar22@users.noreply.github.com>
2024-01-25 20:18:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 //your code here
 
+const input = document.getElementById("newTodoInput")
+const button = document.getElementById("addTodoBtn")
+const list = document.getElementById("todoList")
+const todo = [];
+button.addEventListener("click", ()=>{
+    const taskText = input.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    const taskItem = document.createElement("li");
+    taskItem.className = "task-item";
+    taskItem.innerHTML = `
+        <span>${taskText}</span>
+    `;
+    list.appendChild(taskItem);
+
+    // Clear the input field
+    input.value = "";
+})
